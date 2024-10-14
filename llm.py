36ba5c96a32192faa9c67525a22ea278398a95d6
IbnuEyni <shuaibahmedin@gmail.com>
2024-10-14 14:48:58 +0300

    movie chat bot

--- llm.py
+++ llm.py
@@ -1,6 +1,15 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 import streamlit as st
+from graph import graph
+
+load_dotenv()
+
 
 # Create the LLM
-
+llm = ChatGoogleGenerativeAI(model="gemini-pro",
+                             google_api_key=st.secrets["GOOGLE_API_KEY"] 
+                             )
 
 # Create the Embedding model
